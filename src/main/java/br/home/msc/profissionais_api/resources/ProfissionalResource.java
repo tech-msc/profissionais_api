799f36f67e9a6672d4d4ac26bac4fdfb26a9b86c
tech-msc <matheusscardoso5@github.com>
2020-05-02 14:48:15 -0300

    swagger configurado

--- src/main/java/br/home/msc/profissionais_api/resources/ProfissionalResource.java
+++ src/main/java/br/home/msc/profissionais_api/resources/ProfissionalResource.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,7 +21,11 @@ import br.home.msc.profissionais_api.models.Estabelecimento;
 import br.home.msc.profissionais_api.models.Profissional;
 import br.home.msc.profissionais_api.repository.IEstabelecimentoRepository;
 import br.home.msc.profissionais_api.repository.IProfissionalRepository;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
+@Api(value="API Profissionais", tags="Profissional")
+@CrossOrigin(origins="*")
 @RestController
 @RequestMapping("/api/profissional")
 public class ProfissionalResource {
@@ -32,11 +37,12 @@ public class ProfissionalResource {
 	IEstabelecimentoRepository estabelecimentoRepository;
 
 	@GetMapping()
+	@ApiOperation(value = "Retornar uma lista de Profissionais")
 	public ResponseEntity<List<Profissional>> listAll() {
 		try {
 
 			List<Profissional> profissional_retorno = profissionalRepository.findAll();
-			return new ResponseEntity<List<Profissional>>(profissional_retorno, HttpStatus.FOUND);
+			return new ResponseEntity<List<Profissional>>(profissional_retorno, HttpStatus.OK);
 
 		} catch (Exception e) {
 		}
@@ -46,13 +52,14 @@ public class ProfissionalResource {
 	}
 
 	@GetMapping(value = "/{id}")
+	@ApiOperation(value="Retornar um Profissional")
 	public ResponseEntity<Profissional> listOne(@PathVariable(value = "id") int id) {
 		try {
 
 			Profissional profissional_retorno = profissionalRepository.findById(id);
 
 			if (profissional_retorno != null) {
-				return new ResponseEntity<Profissional>(profissional_retorno, HttpStatus.FOUND);
+				return new ResponseEntity<Profissional>(profissional_retorno, HttpStatus.OK);
 			}
 
 		} catch (Exception e) {
@@ -64,9 +71,10 @@ public class ProfissionalResource {
 	}
 
 	@PostMapping()
+	@ApiOperation(value="Criar um novo Profissional")
 	public ResponseEntity<Profissional> create(@Valid @RequestBody Profissional profissional) {
 		try {
-			return new ResponseEntity<Profissional>(profissionalRepository.save(profissional), HttpStatus.ACCEPTED);
+			return new ResponseEntity<Profissional>(profissionalRepository.save(profissional), HttpStatus.CREATED);
 
 		} catch (Exception e) {
 			// String em = e.getMessage();
@@ -76,6 +84,7 @@ public class ProfissionalResource {
 	}
 
 	@PutMapping(value = "/{id}")
+	@ApiOperation(value="Editar um Profissional")
 	public ResponseEntity<Profissional> update(@PathVariable int id, @RequestBody Profissional profissional) {
 
 		if(profissional.getId() != id) {
@@ -96,6 +105,7 @@ public class ProfissionalResource {
 	}
 
 	@DeleteMapping(value = "/{id}")
+	@ApiOperation(value="Excluir um Profissional")
 	public ResponseEntity<Profissional> delete(@PathVariable int id) {
 
 		Profissional profissionalExiste = profissionalRepository
--- src/main/java/br/home/msc/profissionais_api/resources/TesteResource.java
+++ src/main/java/br/home/msc/profissionais_api/resources/TesteResource.java
@@ -1,14 +1,23 @@
 package br.home.msc.profissionais_api.resources;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiOperation;
+
+@Api(value = "API Profissionais", tags = "Teste")
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/teste")
 public class TesteResource {
 
 	@GetMapping()
+	@ApiOperation(value = "Retorna uma mensagem se a API estiver funcionando")
+	@ApiModelProperty(name = "teste")
 	public String listAll() {
 		return "Api funcionando";
 	}
