5f0aacb14fdb78b0832015791496aeeae2f61506
tech-msc <matheusscardoso5@github.com>
2020-05-06 18:16:34 -0300

    criar profissional atualizado

---
+++ src/main/java/br/home/msc/profissionais_api/models/ProfissionalRequest.java
@@ -0,0 +1,68 @@
+package br.home.msc.profissionais_api.models;
+
+import java.io.Serializable;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+
+public class ProfissionalRequest implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+
+	private Integer id;
+	private String nome;
+	private String endereco;	
+	private Integer estabelecimento_id;
+	
+
+	public Integer getEstabelecimento_id() {		
+		return estabelecimento_id;
+	}
+
+	public void setEstabelecimento_id(int estabelecimento_id) {
+		this.estabelecimento_id = estabelecimento_id;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+
+	public ProfissionalRequest(String nome, 
+			String endereco, 
+			Integer estabelecimento) {
+		super();
+		this.nome = nome;
+		this.endereco = endereco;
+		this.estabelecimento_id =  estabelecimento ;
+	}
+	
+	public ProfissionalRequest() {
+		// TODO Auto-generated constructor stub
+	} 
+
+}
